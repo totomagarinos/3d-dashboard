e899fd5a72b0d8c725d383c97dbf3dc6ce4d3c1b
Juan Cruz Magariños <totomagarinos@gmail.com>
2026-03-18 20:45:30 -0300

    feat(frontend): implement Angular environments with dev/prod separation

--- frontend/src/app/private/materials/services/material.service.ts
+++ frontend/src/app/private/materials/services/material.service.ts
@@ -1,9 +1,49 @@
 import { HttpClient } from '@angular/common/http';
-import { Inject, inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { Material } from '../models/material.model';
+import { catchError, of, throwError } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MaterialService {
-  private http: HttpClient = Inject(HttpClient);
+  #state = signal(new Map<string, Material>());
+
+  baseUrl = environment.apiUrl;
+  http: HttpClient = inject(HttpClient);
+
+  constructor() {
+    this.getMaterials();
+  }
+
+  getMaterials() {
+    this.http
+      .get<Material[]>(this.baseUrl)
+      .pipe(
+        catchError((err) => {
+          console.error('Error fetching materials.', err);
+
+          const fallback = [
+            {
+              _id: 'abcdefg',
+              type: 'PLA',
+              brand: 'Grilon',
+              weight: 1000,
+              price: 20000,
+            },
+          ];
+
+          if (!environment.useFallback) {
+            return throwError(() => err);
+          }
+
+          return of(fallback);
+        }),
+      )
+      .subscribe((result) => {
+        const materialsMap = new Map(result.map((m) => [m._id, m]));
+        this.#state.set(materialsMap);
+      });
+  }
 }
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://tu-api-production.com/api/materials',
+  useFallback: false,
+};
